5c45e5aec63d91447619d860b6f27ba6a5d73390
Sergey Litvinov <slitvinov@gmail.com>
2020-07-02 10:13:00 +0200

    use dynamic memory

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 enum { W = 800 };
@@ -6,15 +7,19 @@ enum { W = 800 };
 int
 main()
 {
-  unsigned char buf[3 * W * W];
+  unsigned char *buf;
   int R;
   int i;
   int j;
   int u;
   int v;
 
+  if ((buf = malloc(3 * W * W * sizeof *buf)) == NULL) {
+    fprintf(stderr, "malloc failed\n");
+    exit(2);
+  }
   for (R = W/10; R < W/2; R++) {
-    memset(buf, 0, sizeof(buf));
+    memset(buf, 0, 3 * W * W * sizeof *buf);
     for (i = 0; i < W; i++)
       for (j = 0; j < W; j++) {
 	u = i - W/2;
@@ -25,8 +30,11 @@ main()
 	  buf[3 * (i * W + j) + 1] = 0xFF;	  
       }
     printf("P6\n%d %d\n255\n", W, W);
-    fwrite(buf, 3 * W, W, stdout);
-    fflush(stdout);
+    if (fwrite(buf, 3 * W, W, stdout) != W)
+      exit(2);
+    if (fflush(stdout) == EOF)
+      exit(2);
   }
+  free(buf);
   return 0;
 }
